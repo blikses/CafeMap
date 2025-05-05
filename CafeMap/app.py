2aa390f97929a4f8d64f9a0ad3b210b2909765d2
Mariia-fed <110707mf@gmail.com>
2025-05-05 20:57:58 +0300

    Add files via upload

---
+++ CafeMap/app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY DATABASE_URL'] = 'sqlite:///bloq.db'
+db = SQLAlchemy(app)
+
+
+'''Создаем колонки в базе даных для добавления локаций'''
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)  # Название локации
+    '''Не больше 300 символов и нельзя ввести пустое значение'''
+    intro = db.Column(db.String(300), nullable=False)
+    '''Описание лоации'''
+    text = db.olumn(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    '''Когда будем выбирать какой-либо объект из этого класса, нам будет выдаваться объект и id'''
+    def __repr__(self):
+        return '<Article> %r' % self.id
+
+
+@app.route('/')
+@app.route('/home')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/locations')
+def locations():
+    return render_template("locations.html")
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User page: ' + name + " - " + str(id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
